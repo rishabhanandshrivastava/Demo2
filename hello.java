d5debb8771987ec2a081eff1587971efc5055497
rishabhanandshrivastava <51355512+rishabhanandshrivastava@users.noreply.github.com>
2019-06-06 10:50:47 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ Class hello
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello Naveen Welcome to Bharat");
+		System.out.println("Hello Naveen,Rishabh,Surojit and Paras Welcome to Bharat");
 	}
 }
